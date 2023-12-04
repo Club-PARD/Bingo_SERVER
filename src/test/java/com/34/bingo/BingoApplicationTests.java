2a05a6556914065d67005a6bef2329cf336509b9
dbseend <shyoon840@gmail.com>
2023-12-04 20:21:09 +0900

    프로젝트 생성

---
+++ src/test/java/com/34/bingo/BingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com4.bingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

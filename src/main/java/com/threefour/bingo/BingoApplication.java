d94a34f0c8e87c8839f7b8eb4468226af3c4de52
dbseend <shyoon840@gmail.com>
2023-12-04 22:37:17 +0900

    application.properties gitignore 추가

--- src/main/java/com/threefour/bingo/BingoApplication.java
+++ src/main/java/com/threefour/bingo/BingoApplication.java
@@ -1,4 +1,4 @@
-package com4.bingo;
+package com.threefour.bingo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BingoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BingoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BingoApplication.class, args);
+    }
 
 }

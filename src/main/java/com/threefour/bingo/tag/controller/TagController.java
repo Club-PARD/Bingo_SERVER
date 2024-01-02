11a6e67665001d87ba9aa8b12c0bdc8beb6f9058
dbseend <shyoon840@gmail.com>
2024-01-02 17:02:58 +0900

    CORS 수정

--- src/main/java/com/threefour/bingo/tag/controller/TagController.java
+++ src/main/java/com/threefour/bingo/tag/controller/TagController.java
@@ -5,16 +5,14 @@ import com.threefour.bingo.tag.domain.Tag;
 import com.threefour.bingo.tag.dto.request.TagListProjectRequest;
 import com.threefour.bingo.tag.service.TagService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/tag")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TagController {
 
     private final TagService tagService;

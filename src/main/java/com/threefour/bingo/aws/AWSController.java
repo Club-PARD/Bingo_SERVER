85933b2855629363b55cc0cf6c9f62cc5ff71096
dbseend <shyoon840@gmail.com>
2024-01-03 23:48:03 +0900

    이미지 불러오기 컨트롤러 추천

--- src/main/java/com/threefour/bingo/aws/AWSController.java
+++ src/main/java/com/threefour/bingo/aws/AWSController.java
@@ -1,7 +1,6 @@
-package com.threefour.bingo.s3;
+package com.threefour.bingo.aws;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -10,31 +9,44 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/upload")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
-public class FileUploadController {
+public class AWSController {
 
     private final AmazonS3 amazonS3;
+    private final AWSService awsService;
 
     @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
 
     @PostMapping("")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
         try {
-            String fileName = file.getOriginalFilename();
-            String fileUrl = "https://" + bucket + "/test/" + fileName;  // 수정: URL 경로에 슬래시 추가
-            ObjectMetadata metadata = new ObjectMetadata();  // 수정: 변수명 수정
-            metadata.setContentType(file.getContentType());
-            metadata.setContentLength(file.getSize());
-            amazonS3.putObject(bucket, fileName, file.getInputStream(), metadata);
+            String fileUrl = awsService.uploadFile(file);
             return ResponseEntity.ok(fileUrl);
         } catch (IOException e) {
-            e.printStackTrace();  // 수정: 예외 처리 개선
+            e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+    //board 이미지 전달
+    @GetMapping("/cards/{category}/images")
+    public List<String> getBoardImages(@PathVariable("category") String category) {
+
+        List<String> boardImageList = awsService.getFileList("board/" + category); //보드는 카테고리 디렉토리가 하나 더있어서
+
+        return boardImageList;
+    }
+
+    //profile 이미지 전달
+    @GetMapping("members/images")
+    public List<String> getProfileImages() {
+        List<String> profileImageList = awsService.getFileList("profile");
+        return profileImageList;
+    }
+
 }
---
+++ src/main/java/com/threefour/bingo/aws/AWSService.java
@@ -0,0 +1,64 @@
+package com.threefour.bingo.aws;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AWSService {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+        String fileUrl = "https://" + bucketName + "/test/" + fileName;
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+
+        amazonS3.putObject(bucketName, fileName, file.getInputStream(), metadata);
+
+        return fileUrl;
+    }
+
+    //S3폴더 내 파일 리스트 전달
+    public List<String> getFileList(String directory) {
+        List<String> fileList = new ArrayList<>();
+
+        ListObjectsV2Request listObjectsV2Request = new ListObjectsV2Request()
+                .withBucketName(bucketName)
+                .withPrefix(directory + "/");  //폴더 경로 지정
+
+        ListObjectsV2Result result = amazonS3.listObjectsV2(listObjectsV2Request);
+        List<S3ObjectSummary> objectSummaries = result.getObjectSummaries();
+
+        for (S3ObjectSummary objectSummary : objectSummaries) {
+            String key = objectSummary.getKey();
+            if (!key.equals(directory + "/")) {
+                fileList.add("https://" + bucketName + ".s3." + region + ".amazonaws.com/" + key);
+            }
+        }
+
+        return fileList;
+    }
+}

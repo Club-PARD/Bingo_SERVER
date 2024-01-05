90ff12fb08863e8ad7586f199096f0ffb4c306b0
dbseend <shyoon840@gmail.com>
2024-01-05 21:49:58 +0900

    reqeust 수정

--- src/main/java/com/threefour/bingo/auth/service/TokenProvider.java
+++ src/main/java/com/threefour/bingo/auth/service/TokenProvider.java
@@ -15,21 +15,23 @@ import java.util.Date;
 @Service
 @Slf4j
 public class TokenProvider {
-    private static final Key SECURITY_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    private static final Key SECURITY_KEY = Keys.hmacShaKeyFor(Keys.secretKeyFor(SignatureAlgorithm.HS512).getEncoded());
 
     public String create(String email) {
         Date exprTime = Date.from(Instant.now().plus(3, ChronoUnit.HOURS));
 
         return Jwts.builder()
-                .signWith(SignatureAlgorithm.HS512, SECURITY_KEY)
-                .setSubject(email).setIssuedAt((new Date())).setExpiration(exprTime)
+                .signWith(SECURITY_KEY, SignatureAlgorithm.HS512)
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(exprTime)
                 .compact();
     }
 
     public String validate(String token) {
-        Claims claims = Jwts.parserBuilder().setSigningKey(SECURITY_KEY).build().parseClaimsJws(token).getBody();
+        Claims claims = Jwts.parser().setSigningKey(SECURITY_KEY).parseClaimsJws(token).getBody();
 
-        log.info("claims: " + claims.getSubject());
+        log.info("Token validation successful. Subject: {}", claims.getSubject());
 
         return claims.getSubject();
     }

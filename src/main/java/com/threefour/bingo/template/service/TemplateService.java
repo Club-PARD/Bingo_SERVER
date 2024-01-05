4dcd70bfa3da541a5a5cd79eeff10b6407d471eb
dbseend <shyoon840@gmail.com>
2024-01-06 04:00:48 +0900

    템플릿 조회 작성 여부 역순

--- src/main/java/com/threefour/bingo/template/service/TemplateService.java
+++ src/main/java/com/threefour/bingo/template/service/TemplateService.java
@@ -32,6 +32,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -118,6 +119,8 @@ public class TemplateService {
             }
 
         }
+
+        Collections.reverse(isWritedList);
 
 //        final List<Template> templateList = templateRepository.findByProjectId(projectId);
 

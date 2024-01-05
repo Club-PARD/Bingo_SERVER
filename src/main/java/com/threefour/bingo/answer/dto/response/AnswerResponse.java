7d17d02326391f7af795d0051728a96513972724
dbseend <shyoon840@gmail.com>
2024-01-06 07:59:08 +0900

    회고 작성자 사진 포함;

--- src/main/java/com/threefour/bingo/answer/dto/response/AnswerResponse.java
+++ src/main/java/com/threefour/bingo/answer/dto/response/AnswerResponse.java
@@ -1,6 +1,7 @@
 package com.threefour.bingo.answer.dto.response;
 
 import com.threefour.bingo.answer.domain.Answer;
+import com.threefour.bingo.appUser.dto.response.AppUserAnsResponse;
 import com.threefour.bingo.appUser.dto.response.AppUserInfoResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,14 +16,14 @@ public class AnswerResponse {
 
     private String ams;
 
-    private String writer;
+//    private String writer;
 
-//    private AppUserInfoResponse appUserInfoResponse;
+    private AppUserAnsResponse appUserAnsResponse;
 
     public AnswerResponse(Answer answer) {
         this.id = answer.getId();
         this.ams = answer.getAns();
-        this.writer = answer.getAppUser().getName();
+        this.appUserAnsResponse = new AppUserAnsResponse(answer.getAppUser().getName(), answer.getAppUser().getPicture());
     }
 
 }
---
+++ src/main/java/com/threefour/bingo/appUser/dto/response/AppUserAnsResponse.java
@@ -0,0 +1,16 @@
+package com.threefour.bingo.appUser.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUserAnsResponse {
+
+    private String name;
+
+    private String picture;
+
+}

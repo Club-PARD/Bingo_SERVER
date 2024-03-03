61340b12ee9517c5710221f63b1e73c290a91ddb
dbseend <shyoon840@gmail.com>
2024-03-04 01:19:09 +0900

    [FEAT]: BaseEntity 생성

---
+++ src/main/java/com/threefour/bingo/appUser/service/AppUserSubService.java
@@ -0,0 +1,32 @@
+package com.threefour.bingo.appUser.service;
+
+import com.threefour.bingo.appUser.domain.AppUser;
+import com.threefour.bingo.appUser.domain.AppUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AppUserSubService {
+
+    private final AppUserRepository appUserRepository;
+
+    // 사용자 id로 레포지토리에서 사용자를 찾아 반환
+    public AppUser findAppUserById(Long appUserId) {
+        return appUserRepository.findById(appUserId)
+                .orElseThrow(() -> new IllegalArgumentException("User Not Found"));
+    }
+
+    // 사용자 이메일로 레포지토리에서 사용자를 찾아 반환
+    public AppUser findAppUserByEmail(String email) {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("User Not Found"));
+    }
+
+    // 사용자 이메일 인증여부 확인
+    public void isEmailVerified(boolean emailVerified) {
+        if (!emailVerified) {
+            throw new IllegalArgumentException("Email is not verified");
+        }
+    }
+}
